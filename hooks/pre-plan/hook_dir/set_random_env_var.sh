dd4300b7f5e5e0023263e8fc2507cc06f184931d
Kostya Kotov <38981440+k-kotov@users.noreply.github.com>
2025-03-12 12:03:18 +0200

    Update set_random_env_var.sh

--- hooks/pre-plan/hook_dir/set_random_env_var.sh
+++ hooks/pre-plan/hook_dir/set_random_env_var.sh
@@ -2,12 +2,11 @@
 
 set -e
 
-chmod -R +x $SCALR_HOOK_DIR
-
 # Generate a random value (1 or 0)
 export ALLOW_RUN=$((RANDOM % 2))
 
 echo "Pre-plan hook: Setting ALLOW_RUN=$ALLOW_RUN"
 
-echo "Calling $SCALR_HOOK_DIR/export_env_vars_to_file.sh..."
-"$SCALR_HOOK_DIR/export_env_vars_to_files.sh"
+echo "Calling ./export_env_vars_to_file.sh..."
+chmod +x $SCALR_HOOK_DIR/export_env_vars_to_file.sh
+$SCALR_HOOK_DIR/export_env_vars_to_file.sh
